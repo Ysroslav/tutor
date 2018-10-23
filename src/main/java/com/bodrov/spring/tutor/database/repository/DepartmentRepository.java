08ca3f68e3334468629e27393dc8a37f34a8a240
Yaroslav <yaroslavbodrov@mail.ru>
2018-10-23 11:03:17 +0300

    Выполнение домашнего задания Spring II lesson -7

--- src/main/java/com/bodrov/spring/tutor/database/repository/DepartmentRepository.java
+++ src/main/java/com/bodrov/spring/tutor/database/repository/DepartmentRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface DepartmentRepository extends JpaRepository<Department, String> {
+
+    public List<Department> findByDepartmentName(String departmentName);
 }
--- src/main/java/com/bodrov/spring/tutor/database/repository/StaffRepository.java
+++ src/main/java/com/bodrov/spring/tutor/database/repository/StaffRepository.java
@@ -1,12 +1,15 @@
 package com.bodrov.spring.tutor.database.repository;
 
-import com.bodrov.spring.tutor.database.entity.Department;
 import com.bodrov.spring.tutor.database.entity.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface StaffRepository extends JpaRepository<Staff, String> {
+
+    public List<Staff> findByFirstName(String firstName);
 }
